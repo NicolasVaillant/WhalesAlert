c2a397dfebf0c19a74109da79c98737a2ca5c362
NicolasVaillant <see_f20nvail@etu.emse.fr>
2023-12-25 22:06:06 +0100

    init

--- parameters/parameters.js
+++ parameters/parameters.js
@@ -1,1 +1,3 @@
-const LINK_TO_DATA = ""
+const LINK_TO_DATA__main = "resources/data/main.json"
+const LINK_TO_DATA__gainers = "resources/data/gainers.json"
+const LINK_TO_DATA__losers = "resources/data/losers.json"
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,42 @@
 window.onload = function () {
-    loadResult()
+    fLoad_main()
         .then(r => {
-            const data = (typeof r === 'object') ? r : "error reading data"
+            const data = (typeof r === 'object') ? r : "error reading data main"
+            console.log(data)
+        })
+    
+    fLoad_gainers()
+        .then(r => {
+            const data = (typeof r === 'object') ? r : "error reading data gainers"
+            console.log(data)
+        })
+        
+    fLoad_losers()
+        .then(r => {
+            const data = (typeof r === 'object') ? r : "error reading data losers"
             console.log(data)
         })
 }
 
-const loadResult = async() => {
+const fLoad_main = async() => {
     try {
-        const response = await fetch(LINK_TO_DATA);
+        const response = await fetch(LINK_TO_DATA__main);
+        return await response.json()
+    } catch (error) {
+        return error.message
+    }
+}
+const fLoad_gainers = async() => {
+    try {
+        const response = await fetch(LINK_TO_DATA__gainers);
+        return await response.json()
+    } catch (error) {
+        return error.message
+    }
+}
+const fLoad_losers = async() => {
+    try {
+        const response = await fetch(LINK_TO_DATA__losers);
         return await response.json()
     } catch (error) {
         return error.message
