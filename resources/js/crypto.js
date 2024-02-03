7f8e29e5e0953ef61c7617f1a4cb7bf729537343
NicolasVaillant <see_f20nvail@etu.emse.fr>
2024-02-03 19:29:30 +0100

    table for crypto

--- resources/js/crypto.js
+++ resources/js/crypto.js
@@ -1,3 +1,53 @@
 const query = decodeURIComponent(window.location.search.split("?q=")[1])
 const name_c = document.querySelector('.name-crypto');
 name_c.innerHTML = `${query.charAt(0).toUpperCase() + query.slice(1)}`
+
+const fLoad_crypto = async() => {
+    try {
+        const response = await fetch(`resources/data/${query}.json`);
+        return await response.json()
+    } catch (error) {
+        return error.message
+    }
+}
+
+fLoad_crypto()
+    .then(r => {
+        const data = (typeof r === 'object') ? r : "No currency found"
+        fLoad_table(r)
+    })
+
+
+const fLoad_table = async(r) => {
+    let leaderboard_table = $('#table_crypto_unique').DataTable({
+        data: r,
+        lengthMenu: [
+            [10, 25, 50],
+            [10, 25, 50],
+        ],
+        columns: [
+            { data: 'amount'},
+            { data: 'value' },
+            { data: 'porcentage_supply' },
+            { data: 'url' }
+        ],
+        columnDefs: [{
+            "defaultContent": "-",
+            "targets": "_all"
+        }],
+        order: [[0, 'asc']],
+        scrollX: "300px",
+        // "paging": pagingAllow,
+        // "lengthChange": lengthChangeAllow,
+        "searching": true,
+        "ordering": true,
+        // "info": pagingAllow,
+        "autoWidth": false,
+        "responsive": false,
+        // "language": translation[languageSelect].dataTable,
+        // "initComplete": function() {
+        //     initComplete_leaderboard = true
+        //     changeImageTable(player.players, this[0].querySelector('tbody'))
+        // }
+    });
+}
