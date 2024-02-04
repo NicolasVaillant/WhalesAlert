191640af2c6d61114820a95c998b5a769f99b2d6
NicolasVaillant <see_f20nvail@etu.emse.fr>
2024-02-04 18:10:05 +0100

    Icon in cyrpto page added + comments

--- resources/js/crypto.js
+++ resources/js/crypto.js
@@ -1,5 +1,6 @@
 const query = decodeURIComponent(window.location.search.split("?q=")[1])
 const name_c = document.querySelector('.name-crypto');
+const logo_crypto = document.querySelector('.logo_crypto');
 name_c.innerHTML = `${query.charAt(0).toUpperCase() + query.slice(1)}`
 
 const fLoad_crypto = async() => {
@@ -10,11 +11,26 @@ const fLoad_crypto = async() => {
         return error.message
     }
 }
+const fLoad_cryptoIMG = async() => {
+    try {
+        const response = await fetch(`resources/logos/${query}.png`);
+        return await response.blob()
+    } catch (error) {
+        return error.message
+    }
+}
 
 fLoad_crypto()
     .then(r => {
-        const data = (typeof r === 'object') ? r : "No currency found"
         fLoad_table(r)
+    })
+
+fLoad_cryptoIMG()
+    .then(r => {
+        const data = (r.type === 'image/png') ? r : "null"
+        if(r.type == 'image/png'){
+            logo_crypto.src = `resources/logos/${query}.png`
+        }
     })
 
 
